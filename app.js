e12b810e342be8c2b42e065b476769891df4e16e
midnamic912 <midnamic912@gmail.com>
2023-01-15 13:33:54 +0800

    Format files by prettier

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
+require("dotenv").config();
 const axios = require("axios");
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const errorHandler = require("./middleware");
 
 const app = express();
@@ -11,11 +11,11 @@ app.use(errorHandler);
 
 // Routes
 
-app.get('/', (req, res)=>{
-    res.send('Welcome to my server for Booking-Smart.')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to my server for Booking-Smart.");
+});
 
-app.get('/merchant', async (req, res) => {
+app.get("/merchant", async (req, res) => {
   const queryKeyword = req.query.keyword;
 
   const fetchPlaceId = async () => {
@@ -24,53 +24,53 @@ app.get('/merchant', async (req, res) => {
         key: process.env.PLACES_API_KEY,
         input: queryKeyword,
         inputtype: "textquery",
-        fields:"name,place_id"
-      }
-    }
-    
+        fields: "name,place_id",
+      },
+    };
+
     // handle zero result
-      const {data: response} = await axios.get("https://maps.googleapis.com/maps/api/place/findplacefromtext/json", config);
-      
-      if (response.status === "ZERO_RESULTS"){
-        return "No Match Result"
-      }
-      return response.candidates[0].place_id;
-    
-}
+    const { data: response } = await axios.get(
+      "https://maps.googleapis.com/maps/api/place/findplacefromtext/json",
+      config
+    );
 
-
-const fetchPlaceDetail = async (placeId) => {
-  const config = {
-    params: {
-      key: process.env.PLACES_API_KEY,
-      place_id: placeId,
-      fields:"name,rating,reviews,user_ratings_total,formatted_address,url",
-      reviews_sort: "newest",
-      language:"zh-TW"
+    if (response.status === "ZERO_RESULTS") {
+      return "No Match Result";
     }
-  }
-  
-    const {data: response} = await axios.get("https://maps.googleapis.com/maps/api/place/details/json", config);
-    console.log('Response logged from server:');
+    return response.candidates[0].place_id;
+  };
+
+  const fetchPlaceDetail = async (placeId) => {
+    const config = {
+      params: {
+        key: process.env.PLACES_API_KEY,
+        place_id: placeId,
+        fields: "name,rating,reviews,user_ratings_total,formatted_address,url",
+        reviews_sort: "newest",
+        language: "zh-TW",
+      },
+    };
+
+    const { data: response } = await axios.get(
+      "https://maps.googleapis.com/maps/api/place/details/json",
+      config
+    );
+    console.log("Response logged from server:");
     console.log(response);
     return response;
-  
-}
-
+  };
 
   const placeId = await fetchPlaceId();
-   
-  if (placeId === "No Match Result"){
-    console.log("The API response with no result.")
-    res.send({result: {name: "No Match Result"}})
+
+  if (placeId === "No Match Result") {
+    console.log("The API response with no result.");
+    res.send({ result: { name: "No Match Result" } });
   } else {
     const resultObj = await fetchPlaceDetail(placeId);
-  res.send(resultObj)
+    res.send(resultObj);
   }
-  
+});
 
-
-
-})
-
-app.listen(8080, ()=>{console.log('Server on port 8080.')});
+app.listen(8080, () => {
+  console.log("Server on port 8080.");
+});
--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
-function errorHandler(err, req, res, next){
-    res.status(500).send("Internal Error.")
+function errorHandler(err, req, res, next) {
+  res.status(500).send("Internal Error.");
 }
 
 module.exports = errorHandler;
